bcb95f4a5550ccf44ed8763154a715314d437bc8
Marko Rajkovic <marko88rajkovic@gmail.com>
2022-05-16 22:59:38 +0200

    final

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,21 @@
 function docReady(fn) {
-  // see if DOM is already available
+  //see if DOM is already available
   if (
     document.readyState === "complete" ||
     document.readyState === "interactive"
   ) {
-    // call on next available tick
+    //call on next available tick
     setTimeout(fn, 1);
   } else {
     document.addEventListener("DOMContentLoaded", fn);
   }
 }
 
-// execute code when document is ready
+//execute code when document is ready
 docReady(() => {
   const accordionHeaders = document.querySelectorAll(".accordion-header");
 
-  // Open close accordion
+  //open close accordion
   accordionHeaders.forEach((accordionHeader) => {
     const target = document.getElementById(
       accordionHeader.getAttribute("target")
@@ -26,46 +26,61 @@ docReady(() => {
     });
   });
 
-  // Search
+  //SEARCH FUNCTIONS
   const searchInput = document.getElementById("search-input");
   const dimensionsSection = document.getElementById("dimensions");
   const metricsSection = document.getElementById("metrics");
   const noResultsPlaceholder = document.querySelector(".no-search-results");
+  const dimensionAccordios = dimensionsSection.querySelectorAll(".accordion");
+  const metricAccordios = metricsSection.querySelectorAll(".accordion");
 
-  const searchSection = (e, section) => {
-    let counter = 0;
+  //serches single section
+  const searchSection = (e, accordionElements, section) => {
+    let accordionCounter = 0;
     const searchValue = e.currentTarget.value.toLowerCase();
-    const accordionElements = section.querySelectorAll(".accordion");
 
-    section.classList.remove("hide");
-    // go trough accordions
+    //go trough accordions
     [...accordionElements].forEach((accordion) => {
+      let columnCounter = 0;
+      accordion.classList.add("open");
       //get all first column text from single accordion
       const firstColEls = accordion.querySelectorAll(
         ".table tbody tr td:first-child p"
       );
-      const firstColTexts = [...firstColEls]
-        .map((text) => {
-          return text.innerHTML.toLowerCase();
-        })
-        .join(" ");
+
+      //loop trough table rows show if there are results else hide
+      firstColEls.forEach((firstCol) => {
+        if (firstCol.innerHTML.toLowerCase().includes(searchValue)) {
+          firstCol.parentElement.parentElement.classList.remove("hide");
+          columnCounter++;
+        } else {
+          firstCol.parentElement.parentElement.classList.add("hide");
+        }
+      });
 
       //if any 1st column conains phrase dont hide accordion else hide accordion
-      if (firstColTexts.includes(searchValue)) {
+      if (columnCounter) {
         accordion.classList.remove("hide");
-        counter++;
+        accordionCounter++;
       } else {
         accordion.classList.add("hide");
       }
     });
     //if any on section's accordions has results dont hide section else hide section
-    counter ? section.classList.remove("hide") : section.classList.add("hide");
-    return counter;
+    accordionCounter
+      ? section.classList.remove("hide")
+      : section.classList.add("hide");
+    return accordionCounter;
   };
 
+  //searches both section
   const search = (e) => {
-    const dimensionsResults = searchSection(e, dimensionsSection);
-    const metricsResults = searchSection(e, metricsSection);
+    const dimensionsResults = searchSection(
+      e,
+      dimensionAccordios,
+      dimensionsSection
+    );
+    const metricsResults = searchSection(e, metricAccordios, metricsSection);
 
     if (!dimensionsResults && !metricsResults) {
       noResultsPlaceholder.classList.add("show");
@@ -74,12 +89,14 @@ docReady(() => {
     }
   };
 
+  //avent listener for search
   searchInput.addEventListener("keyup", search);
 
-  // Expand/Collapse
+  //EXPAND/COLLAPSE SECTION FUNCTIONS
   const expandButtons = document.querySelectorAll(".expand");
   const collapseButtons = document.querySelectorAll(".collapse");
 
+  //expand section on button click
   const expandSection = (e, bool) => {
     const target = document.getElementById(
       e.currentTarget.getAttribute("target")
@@ -93,12 +110,14 @@ docReady(() => {
     });
   };
 
+  //listener for expand
   expandButtons.forEach((btn) =>
     btn.addEventListener("click", (e) => {
       expandSection(e, true);
     })
   );
 
+  //listener for collapse
   collapseButtons.forEach((btn) =>
     btn.addEventListener("click", (e) => {
       expandSection(e, false);
